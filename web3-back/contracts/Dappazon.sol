5e5da17fba4fac73c48961e16005d2885296e96e
Jay Goyal <f20212418@pilani.bits-pilani.ac.in>
2023-01-28 01:00:38 +0530

    Buy and Withdraw functionality

--- web3-back/contracts/Dappazon.sol
+++ web3-back/contracts/Dappazon.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 contract Dappazon {
   event Add(string name, uint256 cost, uint256 quantity);
+  event Buy(address buyer, uint256 orderID, uint256 itemID);
   struct Item {
     uint256 id;
     string name;
@@ -13,8 +14,15 @@ contract Dappazon {
     uint256 stock;
   }
 
+  struct Order {
+    uint256 time;
+    Item item;
+  }
+
   address public owner;
   mapping(uint256 => Item) public items;
+  mapping(address => uint256) public orderCount;
+  mapping(address => mapping(uint256 => Order)) public orders;
 
   modifier onlyOwner() {
     require(msg.sender == owner, "Only owner can call this function");
@@ -34,7 +42,7 @@ contract Dappazon {
     uint256 _cost,
     uint256 _rating,
     uint256 _stock
-  ) public onlyOwner {
+  ) external onlyOwner {
     // Create an item
     Item memory item = Item(
       _id,
@@ -51,5 +59,34 @@ contract Dappazon {
 
     // Emit Add event
     emit Add(_name, _cost, _stock);
+  }
+
+  // Buy Products
+  function buy(uint256 _id) external payable {
+    // Fetch item
+    Item storage item = items[_id];
+
+    // Require a certain amount of eth to be sent and item is in stock
+    require(item.stock > 0);
+    require(msg.value >= item.cost, "Transfer the correct amount of ETH");
+
+    // Create order
+    Order memory order = Order(block.timestamp, item);
+
+    // Save order to chain
+    orderCount[msg.sender]++;
+    orders[msg.sender][orderCount[msg.sender]] = order;
+
+    // Subtract stock
+    items[_id].stock--;
+
+    // Emit buy event
+    emit Buy(msg.sender, orderCount[msg.sender], item.id);
+  }
+
+  // Withdraw funds
+  function withdraw() external onlyOwner {
+    (bool success, ) = owner.call{value: address(this).balance}("");
+    require(success, "Transaction unsuccessful");
   }
 }
--- web3-back/test/Dappazon.ts
+++ web3-back/test/Dappazon.ts
@@ -1,6 +1,6 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { ContractTransaction } from "ethers";
+import { BigNumber, ContractTransaction } from "ethers";
 import { ethers } from "hardhat";
 import { beforeEach } from "mocha";
 import { Dappazon } from "../typechain-types";
@@ -64,6 +64,81 @@ describe("Dappazon", () => {
           .connect(buyer)
           .add(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK)
       ).to.be.revertedWith("Only owner can call this function");
+    });
+  });
+
+  describe("Buying", () => {
+    let transaction: ContractTransaction;
+
+    beforeEach(async () => {
+      // List an item
+      transaction = await dappazon
+        .connect(deployer)
+        .add(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+
+      // Buy an item
+      transaction = await dappazon.connect(buyer).buy(ID, { value: COST });
+    });
+
+    it("Updates buyers order count", async () => {
+      const result = await dappazon.orderCount(buyer.address);
+      expect(result).to.equal(1);
+    });
+
+    it("Adds the order", async () => {
+      const order = await dappazon.orders(buyer.address, 1);
+
+      expect(order.time).to.be.greaterThan(0);
+      expect(order.item.name).to.equal(NAME);
+    });
+
+    it("Updates stock", async () => {
+      const item = await dappazon.items(ID);
+      const order = await dappazon.orders(buyer.address, 1);
+      expect(item.stock).to.equal(STOCK - 1);
+    });
+
+    it("Updates the contract balance", async () => {
+      const result = await ethers.provider.getBalance(dappazon.address);
+      expect(result).to.equal(COST);
+    });
+
+    it("Emits Buy event", async () => {
+      expect(transaction).to.emit(dappazon, "Buy");
+    });
+  });
+
+  describe("Withdrawing", async () => {
+    let balanceBefore: BigNumber;
+
+    beforeEach(async () => {
+      // List an item
+      let transaction = await dappazon
+        .connect(deployer)
+        .add(ID, NAME, CATEGORY, IMAGE, COST, RATING, STOCK);
+      await transaction.wait();
+
+      // Buy an item
+      transaction = await dappazon.connect(buyer).buy(ID, { value: COST });
+      await transaction.wait();
+
+      // Get deployer balance before
+      balanceBefore = await ethers.provider.getBalance(deployer.address);
+
+      // Withdraw
+      transaction = await dappazon.connect(deployer).withdraw();
+      await transaction.wait();
+    });
+
+    it("Updates owner balance", async () => {
+      const balanceAfter = await ethers.provider.getBalance(deployer.address);
+      expect(balanceAfter).to.be.greaterThan(balanceBefore);
+    });
+
+    it("Updates contract balance", async () => {
+      const result = await ethers.provider.getBalance(dappazon.address);
+      expect(result).to.be.equal(0);
     });
   });
 });
